9984ceaa5d2e3c970aff933bc68d286b985d2950
Jacob Shron <jacobshron314@gmail.com>
2026-02-27 19:12:32 -0500

    Add Spring Boot and Maven setup

--- service/src/main/java/codenames/game/Codenames.java
+++ service/src/main/java/codenames/game/Codenames.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Codenames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("""
+        System.out.print("""
                 \nMENU:
                    1: Start New Game
                    2: History
                   \s
-                Enter option: \s""");
+                Enter option:\s""");
 
         int option = scanner.nextInt();
 
